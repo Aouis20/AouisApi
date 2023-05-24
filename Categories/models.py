941a0d3419766d7dfa77b41ce6ebe3018ea66e61
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2023-05-24 11:12:12 +0200

    perf(Accounts): :zap: make category uniques

--- Categories/models.py
+++ Categories/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Category(models.Model):
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
