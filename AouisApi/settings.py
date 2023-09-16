743989a5d3cab3d8e548e3937946f475cdead988
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2023-09-16 13:31:23 +0200

    fix(Settings): :bug: fix configuration variables

--- AouisApi/settings.py
+++ AouisApi/settings.py
@@ -24,12 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*(@a-6l2e_!pg7s4-&cfx3w(7yp#(h%1*ir1+nlll=p4a8%5=v"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 
 # Application definition
 
@@ -109,24 +103,6 @@ WSGI_APPLICATION = "AouisApi.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": "postgres",
-#         "USER": "postgres",
-#         "PASSWORD": "postgres",
-#         "HOST": "postgres",
-#         "PORT": "5432",
-#     }
-# }
-
 DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 # Password validation
@@ -173,8 +149,17 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "Accounts.User"
 
-FRONT_BASE_URL = os.getenv("FRONT_BASE_URL", "http://localhost:3000")
+# CONFIGURATION
 
-ALLOWED_HOSTS = ["aouisapi.onrender.com", "127.0.0.1", "localhost"]
-CORS_ALLOWED_ORIGINS = ["https://aouisapi.onrender.com", "http://localhost:3000"]
-CSRF_TRUSTED_ORIGINS = ["aouisapi.onrender.com", "127.0.0.1", "localhost"]
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+
+FRONT_BASE_URL = os.environ.get("FRONT_BASE_URL", "http://localhost:3000")
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+CORS_ALLOWED_ORIGINS = os.environ.get("ALLOWED_HOSTS").split(" ")
+CSRF_TRUSTED_ORIGINS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
